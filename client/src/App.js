2a5feb09297d4e09ac4180429a8d7c1ee79b335d
Janessa Garrow <janessa.garrow@gmail.com>
2020-03-25 14:21:24 -0600

    Set up create-react-app client

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import BookList from "./components/BookList";
+
+function App() {
+    return (
+        <div id="main">
+            <h1>My Reading List</h1>
+            <BookList />
+        </div>
+    );
+}
+
+export default App;
